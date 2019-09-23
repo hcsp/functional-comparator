6a8730c82defeafec145250db68092c5446f570b
fcbhank <hankhu4work@163.com>
2019-09-23 12:16:45 +0800

    Commit-1 重写compareTo()方法，注意未比较的属性

--- src/main/java/com/github/hcsp/functional/Order.java
+++ src/main/java/com/github/hcsp/functional/Order.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
 
-public class Order {
+public class Order implements Comparable<Order> {
     // 订单编号，全局唯一
     private Integer id;
     // 下单时间
@@ -53,22 +53,46 @@ public class Order {
                 + '}';
     }
 
-    // 请尝试编写一个方法，输入一个订单列表，输出一个TreeSet，TreeSet中订单的排序规则是：
-    // 1.首先按照是否关闭排序，未关闭的订单靠前；
-    // 2.然后按照订单金额排序，订单金额大的靠前；
-    // 3.然后按照下单时间排序，下单时间早的靠前
+    // 请尝试编写一个方法，输入一个订单列表，输出一个TreeSet
     public static TreeSet<Order> toTreeSet(List<Order> orders) {
-        return null;
+        TreeSet<Order> treeSet = new TreeSet<>();
+        if (orders == null || orders.size() == 0) {
+            return new TreeSet<>();
+        }
+        treeSet.addAll(orders);
+        return treeSet;
     }
 
     public static void main(String[] args) {
         Instant now = Instant.now();
-        System.out.println(
-                toTreeSet(
-                        Arrays.asList(
-                                new Order(1, now, false, new BigDecimal("1")),
-                                new Order(2, now.minusSeconds(1), true, new BigDecimal("2")),
-                                new Order(3, now.minusSeconds(-1), true, new BigDecimal("3")),
-                                new Order(4, now.minusSeconds(2), false, new BigDecimal("4")))));
+        TreeSet<Order> result = toTreeSet(
+                Arrays.asList(
+                        new Order(1, now, false, new BigDecimal("1")),
+                        new Order(2, now.minusSeconds(1), true, new BigDecimal("2")),
+                        new Order(3, now.minusSeconds(-1), true, new BigDecimal("3")),
+                        new Order(4, now.minusSeconds(2), false, new BigDecimal("4"))));
+        System.out.println("result.size(): " + result.size());
+        System.out.println(result);
+    }
+
+    // TreeSet中订单的排序规则是：
+    // 1.首先按照是否关闭排序，未关闭的订单靠前；
+    // 2.然后按照订单金额排序，订单金额大的靠前；
+    // 3.然后按照下单时间排序，下单时间早的靠前
+    @Override
+    public int compareTo(Order order) {
+        if (open && !order.open) {
+            return -1;
+        }
+        if (!open && order.open) {
+            return 1;
+        }
+        if (!amount.equals(order.amount)) {
+            return -1 * amount.compareTo(order.amount);
+        }
+        if (!orderTime.equals(order.orderTime)) {
+            return orderTime.compareTo(order.orderTime);
+        }
+        return id.compareTo(order.id);
     }
 }
