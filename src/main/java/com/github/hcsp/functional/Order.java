bf785199ba28436a6ed62669f3179c97564f810d
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-10-21 15:29:50 +0800

    ok (#17)
    ok (#17)

--- src/main/java/com/github/hcsp/functional/Order.java
+++ src/main/java/com/github/hcsp/functional/Order.java
@@ -1,10 +1,9 @@
 package com.github.hcsp.functional;
 
+
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Order {
     // 订单编号，全局唯一
@@ -53,12 +52,20 @@ public class Order {
                 + '}';
     }
 
+
     // 请尝试编写一个方法，输入一个订单列表，输出一个TreeSet，TreeSet中订单的排序规则是：
     // 1.首先按照是否关闭排序，未关闭的订单靠前；
     // 2.然后按照订单金额排序，订单金额大的靠前；
     // 3.然后按照下单时间排序，下单时间早的靠前
     public static TreeSet<Order> toTreeSet(List<Order> orders) {
-        return null;
+        TreeSet<Order> treeSet = new TreeSet<>(Comparator
+                .comparing(Order::isOpen)
+                .thenComparing(Order::getAmount).reversed()
+                .thenComparing(Order::getOrderTime)
+                .thenComparing(Order::getId));
+        treeSet.addAll(orders);
+        return treeSet;
+
     }
 
     public static void main(String[] args) {
@@ -71,4 +78,6 @@ public class Order {
                                 new Order(3, now.minusSeconds(-1), true, new BigDecimal("3")),
                                 new Order(4, now.minusSeconds(2), false, new BigDecimal("4")))));
     }
+
+
 }
