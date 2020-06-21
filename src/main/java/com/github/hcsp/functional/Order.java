7dcd75b1dade5718644d0c22450767e648fb2626
Haixiang <haixiang6123@gmail.com>
2020-06-21 10:57:26 +0800

    函数式Comparator (#88)
    函数式Comparator (#88)

--- src/main/java/com/github/hcsp/functional/Order.java
+++ src/main/java/com/github/hcsp/functional/Order.java
@@ -3,6 +3,7 @@ package com.github.hcsp.functional;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -58,7 +59,15 @@ public class Order {
     // 2.然后按照订单金额排序，订单金额大的靠前；
     // 3.然后按照下单时间排序，下单时间早的靠前
     public static TreeSet<Order> toTreeSet(List<Order> orders) {
-        return null;
+        TreeSet<Order> treeSet = new TreeSet<>(Comparator.comparing(Order::isOpen)
+                .thenComparing(Order::getAmount).reversed()
+                .thenComparing(Order::getOrderTime)
+                .thenComparing(Order::getId)
+        );
+
+        treeSet.addAll(orders);
+
+        return treeSet;
     }
 
     public static void main(String[] args) {
