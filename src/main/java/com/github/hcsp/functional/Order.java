4b08e43676c303178f815217d90c4a261ccba9d3
Ryan <2433005622@qq.com>
2020-08-07 19:31:55 +0800

    使用函数式Comparator排序 1.使用Comparator创建TreeSet排序规则 2.因为没有添加id的排序规则，当其他属性相同时，判断重复的过程中产生数据丢失。

--- src/main/java/com/github/hcsp/functional/Order.java
+++ src/main/java/com/github/hcsp/functional/Order.java
@@ -2,7 +2,11 @@ package com.github.hcsp.functional;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.TreeSet;
+
 
 public class Order {
     // 订单编号，全局唯一
