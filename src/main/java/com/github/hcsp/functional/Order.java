1059834e67afffeb67ea67ccf1f93a130bbb182d
小虾米 <freebufer.wang@gmail.com>
2021-11-16 03:25:27 +0800

    Functional comparator test (#132)
    Functional comparator test (#132)

--- src/main/java/com/github/hcsp/functional/Order.java
+++ src/main/java/com/github/hcsp/functional/Order.java
@@ -3,10 +3,11 @@ package com.github.hcsp.functional;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
 
-public class Order {
+public class Order implements Comparable<Order> {
     // 订单编号，全局唯一
     private Integer id;
     // 下单时间
@@ -58,7 +59,7 @@ public class Order {
     // 2.然后按照订单金额排序，订单金额大的靠前；
     // 3.然后按照下单时间排序，下单时间早的靠前
     public static TreeSet<Order> toTreeSet(List<Order> orders) {
-        return null;
+        return new TreeSet<>(orders);
     }
 
     public static void main(String[] args) {
@@ -70,5 +71,19 @@ public class Order {
                                 new Order(2, now.minusSeconds(1), true, new BigDecimal("2")),
                                 new Order(3, now.minusSeconds(-1), true, new BigDecimal("3")),
                                 new Order(4, now.minusSeconds(2), false, new BigDecimal("4")))));
+    }
+
+    @Override
+    public int compareTo(Order o) {
+        Comparator<Order> orderComparator = createOrderComparator();
+        int result = orderComparator.compare(this, o);
+        if (result == 0) {
+            return id - o.id;
+        }
+        return result;
+    }
+
+    private Comparator<Order> createOrderComparator() {
+        return Comparator.comparing(Order::isOpen).thenComparing(Order::getAmount).reversed().thenComparing(Order::getOrderTime);
     }
 }
