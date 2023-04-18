16565b1188be9b69244f845805b57a02c09d4356
wjxalexander <37684072+wjxalexander@users.noreply.github.com>
2023-04-18 22:38:40 +0800

    homework done (#146)
    homework done (#146)

--- src/main/java/com/github/hcsp/functional/Order.java
+++ src/main/java/com/github/hcsp/functional/Order.java
@@ -2,11 +2,9 @@ package com.github.hcsp.functional;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
-public class Order {
+public class Order implements Comparable<Order> {
     // 订单编号，全局唯一
     private Integer id;
     // 下单时间
@@ -41,16 +39,7 @@ public class Order {
 
     @Override
     public String toString() {
-        return "Order{"
-                + "id="
-                + id
-                + ", orderTime="
-                + orderTime
-                + ", open="
-                + open
-                + ", amount="
-                + amount
-                + '}';
+        return "Order{" + "id=" + id + ", orderTime=" + orderTime + ", open=" + open + ", amount=" + amount + '}';
     }
 
     // 请尝试编写一个方法，输入一个订单列表，输出一个TreeSet，TreeSet中订单的排序规则是：
@@ -58,17 +47,17 @@ public class Order {
     // 2.然后按照订单金额排序，订单金额大的靠前；
     // 3.然后按照下单时间排序，下单时间早的靠前
     public static TreeSet<Order> toTreeSet(List<Order> orders) {
-        return null;
+        Collections.sort(orders);
+        return new TreeSet<>(orders);
     }
 
     public static void main(String[] args) {
         Instant now = Instant.now();
-        System.out.println(
-                toTreeSet(
-                        Arrays.asList(
-                                new Order(1, now, false, new BigDecimal("1")),
-                                new Order(2, now.minusSeconds(1), true, new BigDecimal("2")),
-                                new Order(3, now.minusSeconds(-1), true, new BigDecimal("3")),
-                                new Order(4, now.minusSeconds(2), false, new BigDecimal("4")))));
+        System.out.println(toTreeSet(Arrays.asList(new Order(1, now, false, new BigDecimal("1")), new Order(2, now.minusSeconds(1), true, new BigDecimal("2")), new Order(3, now.minusSeconds(-1), true, new BigDecimal("3")), new Order(4, now.minusSeconds(2), false, new BigDecimal("4")))));
+    }
+
+    @Override
+    public int compareTo(Order other) {
+        return Comparator.comparing(Order::isOpen).thenComparing(Order::getAmount).reversed().thenComparing(Order::getOrderTime).thenComparing(Order::getId).compare(this, other);
     }
 }
